e0adb7bc370dc86b11ac570f2175aa3fd057871a
faramade124 <kunlestic89@gmail.com>
2025-07-08 17:15:54 +0100

    third  commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 // import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
